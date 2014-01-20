3dc5f7c491654349644adfbc6fe8cce2b7c1b3a6
carolynvs <me@carolynvanslyck.com>
2014-01-19 18:00:58 -0600

    Atlassian Plugin SDK 4.2.12

---
+++ atlassian-plugin-sdk/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'atlassian-plugin-sdk'
+$installerType = 'exe'
+$url = 'https://marketplace.atlassian.com/download/plugins/atlassian-plugin-sdk-windows'
+$silentArgs = '--mode unattended'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
